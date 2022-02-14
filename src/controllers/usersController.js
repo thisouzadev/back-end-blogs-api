0f4e4f76820d68851ea33522b02339eaffcd97b9
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-02-14 08:23:26 -0300

    fix requisito 1

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -12,7 +12,9 @@ const {
 const schemaUser = Joi.object({
   displayName: Joi.string().min(8).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(6).required().messages({
+    'string.min': '"password" length must be 6 characters long',
+  }),
   image: Joi.string().required(),
 });
 
